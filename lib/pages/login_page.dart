93c6a61866b45e6069b0a5a0f94cd4be0b53c08f
aayushchauhan17 <aayushchauhan176@gmail.com>
2023-08-08 14:23:44 +0530

    animmation added

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,8 +1,32 @@
 import "package:flutter/material.dart";
 import "package:flutter_catalog/utils/my_routes.dart";
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String name = "";
+  bool changeButton = false;
+
+  final _formKey = GlobalKey<FormState>();
+
+  Future<void> onPressFunction(BuildContext context) async {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        changeButton = true;
+      });
+      await Future.delayed(const Duration(seconds: 2));
+
+      // await Navigator.pushNamed(context, MyRoutes.home);
+      setState(() {
+        changeButton = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,63 +34,106 @@ class LoginPage extends StatelessWidget {
       color: Colors.white,
       child: Center(
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 0.0,
-              ),
-              Image.asset(
-                "assets/images/login.png",
-                fit: BoxFit.cover,
-                height: 200.0,
-              ),
-              const SizedBox(
-                height: 20.0,
-              ),
-              const Text(
-                "Welcome Boiii",
-                style: TextStyle(
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold,
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 0.0,
                 ),
-              ),
-              const SizedBox(
-                height: 20.0,
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 16.0,
-                  horizontal: 32.0,
+                Image.asset(
+                  "assets/images/login.png",
+                  fit: BoxFit.cover,
+                  height: 200.0,
                 ),
-                child: Column(
-                  children: [
-                    TextFormField(
-                      decoration: const InputDecoration(
-                        hintText: "Enter your Name",
-                        labelText: "Username",
-                      ),
-                    ),
-                    TextFormField(
-                      obscureText: true,
-                      decoration: const InputDecoration(
-                        hintText: "Enter Password",
-                        labelText: "Password",
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 20.0,
-                    ),
-                    ElevatedButton(
-                        onPressed: () {
-                          print("hii tmc");
-
-                          Navigator.pushNamed(context, MyRoutes.home);
+                const SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  'Welcome $name Boiii',
+                  style: const TextStyle(
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 16.0,
+                    horizontal: 32.0,
+                  ),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          hintText: "Enter your Name",
+                          labelText: "Username",
+                        ),
+                        onChanged: (value) {
+                          setState(() {
+                            name = value;
+                          });
                         },
-                        child: const Text("Login"))
-                  ],
-                ),
-              )
-            ],
+                        validator: (String? value) {
+                          if (value != null && value.isEmpty) {
+                            return ("Please Enter the Name.");
+                          } else {
+                            return null;
+                          }
+                        },
+                      ),
+                      TextFormField(
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                          hintText: "Enter Password",
+                          labelText: "Password",
+                        ),
+                        validator: (value) {
+                          if (value != null && value.isEmpty) {
+                            return "Please Enter the Password";
+                          } else if (value != null && value.length < 6) {
+                            return "Please should be atleast 6 digit";
+                          }
+
+                          return null;
+                        },
+                      ),
+                      const SizedBox(
+                        height: 20.0,
+                      ),
+                      InkWell(
+                        onTap: () => onPressFunction(context),
+                        child: AnimatedContainer(
+                          duration: const Duration(seconds: 1),
+                          width: changeButton ? 40 : 100,
+                          height: 40,
+                          alignment: Alignment.center,
+                          decoration: BoxDecoration(
+                            color: Colors.purple,
+                            borderRadius:
+                                BorderRadius.circular(changeButton ? 50 : 8),
+                          ),
+                          child: changeButton
+                              ? const Icon(
+                                  Icons.done,
+                                  color: Colors.white,
+                                )
+                              : const Text(
+                                  "Login",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
